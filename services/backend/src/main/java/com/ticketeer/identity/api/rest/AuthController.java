53269acea7789b15bf3d28c44117cc749ec593a3
Rihabe Mounaim <rihabatimounaim@gmail.com>
2026-04-02 17:40:15 +0200

    feat(api): add auth controller scaffold

---
+++ services/backend/src/main/java/com/ticketeer/identity/api/rest/AuthController.java
@@ -0,0 +1,13 @@
+package com.ticketeer.identity.api.rest;
+
+/**
+ * REST entry point for authentication operations.
+ *
+ * TODO:
+ * - expose POST /auth/login
+ * - map request DTO to AuthenticateUserCommand
+ * - delegate to AuthenticateUserUseCase
+ * - map token response
+ */
+public class AuthController {
+}
