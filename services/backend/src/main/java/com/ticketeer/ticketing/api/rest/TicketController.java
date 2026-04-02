65176e7987ce78a9024f622a78d2ba921360f69c
Rihabe Mounaim <rihabatimounaim@gmail.com>
2026-04-02 17:40:33 +0200

    feat(api): add ticket controller scaffold

---
+++ services/backend/src/main/java/com/ticketeer/ticketing/api/rest/TicketController.java
@@ -0,0 +1,13 @@
+package com.ticketeer.ticketing.api.rest;
+
+/**
+ * REST entry point for ticket operations.
+ *
+ * TODO:
+ * - expose POST /tickets
+ * - map request DTO to IssueTicketCommand
+ * - call IssueTicketUseCase
+ * - return ticket response
+ */
+public class TicketController {
+}
