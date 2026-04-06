32545802ef6cde994e2002e4bbde4934baf558b5
Rihabe Mounaim <rihabatimounaim@gmail.com>
2026-04-06 21:27:46 +0200

    Update MyTicketResponse.java

--- services/backend/src/main/java/com/ticketeer/ticketing/api/rest/MyTicketResponse.java
+++ services/backend/src/main/java/com/ticketeer/ticketing/api/rest/MyTicketResponse.java
@@ -3,6 +3,11 @@ package com.ticketeer.ticketing.api.rest;
 public record MyTicketResponse(
         String ticketId,
         String holderId,
+        String departureStationCode,
+        String arrivalStationCode,
+        String departureTime,
+        String arrivalTime,
+        double price,
         String validFrom,
         String validUntil,
         String status,
