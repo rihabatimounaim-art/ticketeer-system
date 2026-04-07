1233f20e205bc94ed96b432a5ee94a8558eadead
Rihabe Mounaim <rihabatimounaim@gmail.com>
2026-04-07 10:08:55 +0200

    Update CreateTicketRequest.java

--- services/backend/src/main/java/com/ticketeer/ticketing/api/rest/CreateTicketRequest.java
+++ services/backend/src/main/java/com/ticketeer/ticketing/api/rest/CreateTicketRequest.java
@@ -3,6 +3,11 @@ package com.ticketeer.ticketing.api.rest;
 public record CreateTicketRequest(
         String holderId,
         String validFrom,
-        String validUntil
+        String validUntil,
+        String departureStationCode,
+        String arrivalStationCode,
+        String departureTime,
+        String arrivalTime,
+        double price
 ) {
 }
